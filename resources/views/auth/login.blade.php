bebfb12c92796edcca023f1abe1ffe479e61a809
shehabalqudiry <omran.shehab40@gmail.com>
2021-08-11 05:30:35 +0200

    Edit Some fix in dashboard ui

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,14 +6,6 @@
 @section('content')
 		<div class="container-fluid">
 			<div class="row no-gutter">
-				<!-- The image half -->
-				<div class="col-md-6 col-lg-6 col-xl-7 d-none d-md-flex bg-primary-transparent">
-					<div class="row wd-100p mx-auto text-center">
-						<div class="col-md-12 col-lg-12 col-xl-12 my-auto mx-auto wd-100p">
-							<img src="{{URL::asset('assets/img/media/login.png')}}" class="my-auto ht-xl-80p wd-md-100p wd-xl-80p mx-auto" alt="logo">
-						</div>
-					</div>
-				</div>
 				<!-- The content half -->
 				<div class="col-md-6 col-lg-6 col-xl-5 bg-white">
 					<div class="login d-flex align-items-center py-2">
@@ -22,7 +14,7 @@
 							<div class="row">
 								<div class="col-md-10 col-lg-10 col-xl-9 mx-auto">
 									<div class="card-sigin">
-										<div class="mb-5 d-flex"> <a href="{{ url('/' . $page='index') }}"><img src="{{URL::asset('assets/img/brand/favicon.png')}}" class="sign-favicon ht-40" alt="logo"></a><h1 class="main-logo1 ml-1 mr-0 my-auto tx-28">Va<span>le</span>x</h1></div>
+										<div class="mb-5 d-flex"> <a href="{{ url('/' . $page='index') }}"><img src="{{URL::asset('assets/img/brand/favicon.png')}}" class="sign-favicon ht-40" alt="logo"></a><h1 class="main-logo1 ml-1 mr-0 my-auto tx-28">Invoice<span>System</span></h1></div>
 										<div class="card-sigin">
 											<div class="main-signup-header">
 												<h2>مرحبا بعودتك</h2>
@@ -54,12 +46,12 @@
                                                                 <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
                                                                 <label class="form-check-label mr-4" for="remember">
-                                                                    تذكرني  
+                                                                    تذكرني
                                                                 </label>
                                                             </div>
                                                         </div>
                                                     </div>
-                                                    
+
                                                     <button class="btn btn-main-primary btn-block">الدخول</button>
 												</form>
 											</div>
@@ -70,6 +62,15 @@
 						</div><!-- End -->
 					</div>
 				</div><!-- End -->
+                <!-- The image half -->
+                <div class="col-md-6 col-lg-6 col-xl-7 d-none d-md-flex bg-primary-transparent">
+                    <div class="row wd-100p mx-auto text-center">
+                        <div class="col-md-12 col-lg-12 col-xl-12 my-auto mx-auto wd-100p">
+                            <img src="{{URL::asset('assets/img/media/login.png')}}"
+                                class="my-auto ht-xl-80p wd-md-100p wd-xl-80p mx-auto" alt="logo">
+                        </div>
+                    </div>
+                </div>
 			</div>
 		</div>
 @endsection
--- resources/views/layouts/main-header.blade.php
+++ resources/views/layouts/main-header.blade.php
@@ -254,16 +254,18 @@
 										<div class="d-flex wd-100p">
 											<div class="main-img-user"><img alt="" src="{{URL::asset('assets/img/faces/6.jpg')}}" class=""></div>
 											<div class="mr-3 my-auto">
-												<h6>Petey Cruiser</h6><span>Premium Member</span>
+												<h6>{{ auth()->user()->name }}</h6><span>{{ auth()->user()->email }}</span>
 											</div>
 										</div>
 									</div>
-									<a class="dropdown-item" href=""><i class="bx bx-user-circle"></i>Profile</a>
-									<a class="dropdown-item" href=""><i class="bx bx-cog"></i> Edit Profile</a>
-									<a class="dropdown-item" href=""><i class="bx bxs-inbox"></i>Inbox</a>
-									<a class="dropdown-item" href=""><i class="bx bx-envelope"></i>Messages</a>
-									<a class="dropdown-item" href=""><i class="bx bx-slider-alt"></i> Account Settings</a>
-									<a class="dropdown-item" href="{{ url('/' . $page='page-signin') }}"><i class="bx bx-log-out"></i> Sign Out</a>
+									<a class="dropdown-item" href=""><i class="bx bx-user-circle"></i>الصفحة الشخصية</a>
+									<a class="dropdown-item" href=""><i class="bx bx-cog"></i> تعديل الصفحة الشخصية</a>
+									<a class="dropdown-item" href=""><i class="bx bx-envelope"></i>الرسائل</a>
+									<a class="dropdown-item" href=""><i class="bx bx-slider-alt"></i> اعدادات الحساب</a>
+									<a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();"><i class="bx bx-log-out"></i> تسجيل خروج</a>
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
 								</div>
 							</div>
 							<div class="dropdown main-header-message right-toggle">
